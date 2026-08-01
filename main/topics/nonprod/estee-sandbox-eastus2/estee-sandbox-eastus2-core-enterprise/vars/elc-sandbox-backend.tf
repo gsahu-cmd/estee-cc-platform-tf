a511fa876773928ca3c577c7836df0b1ed921b76
EC2 Default User <ec2-user@ip-10-1-3-213.ap-south-1.compute.internal>
2026-08-01 10:47:06 +0000

    topic vars

---
+++ main/topics/nonprod/estee-sandbox-eastus2/estee-sandbox-eastus2-core-enterprise/vars/elc-sandbox-backend.tf
@@ -0,0 +1,4 @@
+resource_group_name  = "estee-poc-tf"
+storage_account_name = "tfstateeesteepoc"
+container_name = "tfstate"
+key = "estee-sandbox-eastus2.estee-sandbox-eastus2-core-enterprise.topic.terraform.tfstate"
