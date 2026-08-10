a66df934d36032d2043c239ee1a677f8ea4a5abe
EC2 Default User <ec2-user@ip-10-1-3-213.ap-south-1.compute.internal>
2026-08-10 12:49:36 +0000

    backend check in

--- main/topics/nonprod/elc-nonprod-eastus2-test/elc-nonprod-eastus2-test-core-enterprise/vars/backend.tf
+++ main/topics/nonprod/elc-nonprod-eastus2-test/elc-nonprod-eastus2-test-core-enterprise/vars/backend.tf
@@ -1,4 +1,4 @@
 resource_group_name  = "estee-poc-tf"
 storage_account_name = "tfstateeesteepoc"
 container_name = "tfstate"
-key = "elc-sandbox.elc-sandbox-eastus2-core-basic.topic.terraform.tfstate"
+key = "elc-nonprod-eastus2-test.elc-nonprod-eastus2-test-core-enterprise.topic.terraform.tfstate"
